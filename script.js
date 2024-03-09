e7389e700590b1867e70acfcc25ff4ec8615de29
Guigo <rsbecker@inf.ufsm.br>
2024-03-09 09:42:36 -0300

    Corrigir estilo

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-const citacaoElemento = document.getElementById("citacaoElemento");
+const citacaoElemento = document.getElementById('citacaoElemento');
 
 const citacoes = [
   'a',
   'b',
   'c'
 ];
+
+const indicesUsados = new Set();
 
 function gerarCitacao() {
   const indice = Math.floor(Math.random() * citacoes.length);
